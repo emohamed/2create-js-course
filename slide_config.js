ca70c7b290c0797d90937332c5639418c48e9640
Emil Mohamed <emil@2c-studio.com>
2013-11-21 10:02:30 +0200

    Changed font to consolas, changed the favicon as well as the presentation title.

--- slide_config.js
+++ slide_config.js
@@ -3,19 +3,19 @@ var SLIDE_CONFIG = {
   settings: {
     title: 'Javascript Basics',
     subtitle: '',
-    // eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    // },
+    eventInfo: {
+     title: '2create js course'
+     // date: '6s/x/2013'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: false, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    // favIcon: '',
+    favIcon: 'http://2create.bg/css/images/favicon.ico',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
-      'Source Code Pro'
+      'Consolas'
     ],
     //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
